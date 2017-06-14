0c1770cc1708f0440ed644467146b09e50754f38
x <294797097@qq.com>
2017-06-14 23:14:21 +0800

    init file

---
+++ anchor-api-server/src/main/java/com/anchor/api/Test.java
@@ -0,0 +1,14 @@
+package com.anchor.api;
+/**
+ * Created by apple on 2017/6/14.
+ */
+
+/**
+ * @author apple
+ * @ClassName: Test
+ * @Description:
+ * @date 2017/6/14 23:13
+ * @since version 1.0
+ */
+public class Test {
+}
